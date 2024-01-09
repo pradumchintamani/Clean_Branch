11ced89092bd49d2109d1c3edd596141ac3880ff
Pradum Chintamani <80237284+pradumchintamani@users.noreply.github.com>
2024-01-09 15:34:19 +0530

    Update delete_branches.sh

--- .github/scripts/delete_branches.sh
+++ .github/scripts/delete_branches.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 # Clone the code repo
-git clone https://github.com/your/repo.git
-cd repo
+git clone https://github.com/pradumchintamani/Clean_Branch.git
+cd Clean_Branch
 
 # Get the list of branches older than x days
-OLD_BRANCHES=$(git for-each-ref --format '%(committerdate:iso8601)%09%(refname:short)' | sort | awk -v cutoff="$(date -d 'x days ago' --iso-8601=seconds)" '$1 < cutoff {print $2}')
+OLD_BRANCHES=$(git for-each-ref --format '%(committerdate:iso8601)%09%(refname:short)' | sort | awk -v cutoff="$(date -d '10 days ago' --iso-8601=seconds)" '$1 < cutoff {print $2}')
 
 # Approval step - you may need to implement your own logic here
 # This is a basic example using GitHub API to get branch protection details
